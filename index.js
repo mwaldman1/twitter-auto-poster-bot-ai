57a6b1992cd6b9f9872deeaa0476391642536b3d
mwaldman1 <134335990+mwaldman1@users.noreply.github.com>
2025-02-04 08:17:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a intersting data point from any poll in this link https://projects.fivethirtyeight.com/polls/ as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    "generate a intersting data point from any poll in this link https://projects.fivethirtyeight.com/polls/ as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis but not #, include the link to the poll";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
